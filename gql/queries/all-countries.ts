e0e01e0723dd73ec5d1b75021641cde7206ecf2d
Benjamin VIELLARD <bicou@bicou.com>
2021-10-15 07:39:21 +0200

    list as table and filter with graphql variables

--- gql/queries/all-countries.ts
+++ gql/queries/all-countries.ts
@@ -1,16 +1,18 @@
 import type * as Types from '../schema';
 
 import type { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
-export type AllCountriesQueryVariables = Types.Exact<{ [key: string]: never; }>;
+export type AllCountriesQueryVariables = Types.Exact<{
+  filter?: Types.Maybe<Types.CountryFilterInput>;
+}>;
 
 
 export type AllCountriesQuery = (
   { __typename?: 'Query' }
   & { countries: Array<(
     { __typename?: 'Country' }
-    & Pick<Types.Country, 'code' | 'name'>
+    & Pick<Types.Country, 'code' | 'name' | 'emoji'>
   )> }
 );
 
 
-export const AllCountriesDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"allCountries"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"countries"},"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"code"}},{"kind":"Field","name":{"kind":"Name","value":"name"}}]}}]}}]} as unknown as DocumentNode<AllCountriesQuery, AllCountriesQueryVariables>;
+export const AllCountriesDocument = {"kind":"Document","definitions":[{"kind":"OperationDefinition","operation":"query","name":{"kind":"Name","value":"allCountries"},"variableDefinitions":[{"kind":"VariableDefinition","variable":{"kind":"Variable","name":{"kind":"Name","value":"filter"}},"type":{"kind":"NamedType","name":{"kind":"Name","value":"CountryFilterInput"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","alias":{"kind":"Name","value":"countries"},"name":{"kind":"Name","value":"countries"},"arguments":[{"kind":"Argument","name":{"kind":"Name","value":"filter"},"value":{"kind":"Variable","name":{"kind":"Name","value":"filter"}}}],"selectionSet":{"kind":"SelectionSet","selections":[{"kind":"Field","name":{"kind":"Name","value":"code"}},{"kind":"Field","name":{"kind":"Name","value":"name"}},{"kind":"Field","name":{"kind":"Name","value":"emoji"}}]}}]}}]} as unknown as DocumentNode<AllCountriesQuery, AllCountriesQueryVariables>;
--- pages/countries.vue
+++ pages/countries.vue
@@ -1,13 +1,23 @@
 <template>
   <section class="pt-5">
+    <input type="text" v-model.trim="code" placeholder="search code" />
     <div v-if="data">
-      <pre>{{ data }}</pre>
+      <table>
+        <tbody>
+        <tr v-for="c in data.countries" :key="c.code">
+          <td>{{ c.name }}</td>
+          <td>{{ c.emoji }}</td>
+        </tr>
+        </tbody>
+      </table>
     </div>
   </section>
 </template>
 
 <script setup lang="ts">
 import {AllCountriesDocument} from "../gql/queries/all-countries";
+
+const code = ref('')
 
 /*
 import {useNuxtApp} from "#app";
@@ -21,7 +31,11 @@ import {useClientHandle} from "@urql/vue";
 
 const urql = useClientHandle();
 
-const { data } = await urql.useQuery({query: AllCountriesDocument})
+const variables = computed(() => {
+  return code.value ? {filter: {code: {eq: code.value.toUpperCase()}}} : {}
+})
+
+const { data } = await urql.useQuery({query: AllCountriesDocument, variables})
 
 /*
 const data = await useAsyncData('countriesg', ctx => {
