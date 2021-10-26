5360d38210be02b8e8d143866ba9886f11b004d9
Benjamin VIELLARD <gbicou@gmail.com>
2021-10-26 12:59:24 +0200

    @/assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 import { defineNuxtConfig } from "nuxt3";
 
 export default defineNuxtConfig({
-  css: ["assets/tailwind.css"],
+  css: ["@/assets/tailwind.css"],
   build: {
     postcss: {
       postcssOptions: {
