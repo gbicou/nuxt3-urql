23274c25887045d682f017b5661d6e6aa3d8cd0d
Benjamin VIELLARD <gbicou@gmail.com>
2022-06-22 17:21:43 +0200

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,8 +17,11 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:vue/vue3-strongly-recommended",
+    "@vue/typescript/recommended",
+    "@vue/eslint-config-typescript/recommended",
   ],
   rules: {
+    "vue/multi-word-component-names": "off",
     "vue/singleline-html-element-content-newline": "off",
     "vue/max-attributes-per-line": ["error", {"singleline": 3}]
   },
