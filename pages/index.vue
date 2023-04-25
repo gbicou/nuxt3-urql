bf10d49a44e21f2663b784153dc768a7375d419d
Benjamin VIELLARD <gbicou@gmail.com>
2023-04-25 11:03:14 +0200

    chore: change username

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <section>
     <p>Demo of nuxt3 SSR querying a graphql api</p>
     <p>
-      <a href="https://github.com/bicouy0/nuxt3-urql" target="_blank" rel="noopener">Source code on github</a>
+      <a href="https://github.com/gbicou/nuxt3-urql" target="_blank" rel="noopener">Source code on github</a>
     </p>
   </section>
 </template>
